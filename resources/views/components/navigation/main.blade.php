930d249201f398ccfec57294fa620e2c3008c456
Matt Banner <matt@banner.wtf>
2020-05-17 19:26:33 +0100

    Describe button

--- resources/views/components/navigation/main.blade.php
+++ resources/views/components/navigation/main.blade.php
@@ -1,6 +1,6 @@
 <nav class="slide slide-top-200 flex items-center justify-between flex-wrap bg-blue-900">
     <div class="flex md:hidden w-full justify-end py-2">
-        <button onclick="toggleMenu()" class="flex items-center px-3 py-2 text-white border-white hover:text-gray-300 transition-fade mr-2">
+        <button onclick="toggleMenu()" aria-label="Expand menu" class="flex items-center px-3 py-2 text-white border-white hover:text-gray-300 transition-fade mr-2">
             <i class="fas fa-bars text-xl"></i>
         </button>
     </div>
