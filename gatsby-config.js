13961133901cbb4dafffc382774a944a51baeb70
Matt Banner <matt@banner.wtf>
2021-02-15 21:27:31 +0000

    Add Vue to technologies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Matt Banner`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Web Developer based in Essex. Working with HTML, CSS, PHP, Laravel, Craft CMS, Vue.js, Docker & AWS.`,
     author: `Matt Banner`,
   },
   plugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <div>
       <h1 className={`text-4xl font-bold mb-2`}>Matt Banner</h1>
       <p className={`text-lg`}>👋🏼 Hello, I'm Matt, a Web Developer and AWS Architect based in the UK.</p>
-      <p className={`text-gray-500 text-sm`}>I work with AWS, Laravel, Craft CMS &amp; Gatsby.</p>
+      <p className={`text-gray-500 text-sm`}>I work with AWS, Laravel, Craft CMS, Vue &amp; Gatsby.</p>
       
       <div className={`mt-6`}>
         <a
