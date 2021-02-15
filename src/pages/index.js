8bf8cb2264384896e6b27476dec39679753e6e68
Matt Banner <matt@banner.wtf>
2021-02-15 21:25:52 +0000

    Remove commented out links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -11,20 +10,6 @@ const IndexPage = () => (
       <h1 className={`text-4xl font-bold mb-2`}>Matt Banner</h1>
       <p className={`text-lg`}>👋🏼 Hello, I'm Matt, a Web Developer and AWS Architect based in the UK.</p>
       <p className={`text-gray-500 text-sm`}>I work with AWS, Laravel, Craft CMS &amp; Gatsby.</p>
-
-      {/* <p className={`font-semibold text-sm`}>
-        <a 
-          href="https://www.enovate.co.uk"
-          className={`text-gray-600 hover:text-gray-800 transition duration-150 ease-in-out`}
-          target={`_blank`}
-        >@Enovate</a>
-
-        <a
-          href="https://www.opworks.co.uk"
-          className={`pl-1 text-gray-600 hover:text-gray-800 transition duration-150 ease-in-out`}
-          target={`_blank`}
-        >@Opworks</a>
-      </p> */}
       
       <div className={`mt-6`}>
         <a
@@ -34,11 +19,6 @@ const IndexPage = () => (
       </div>
 
     </div>
-
-      
-    
-    
-
   </Layout>
 )
 
