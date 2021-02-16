14b32fb4f9b98ff9a15ba296ddf17a7bacc0a6dd
Matt Banner <matt@banner.wtf>
2021-02-16 22:06:55 +0000

    Link the title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
 
     <div>
-      <h1 className={`text-4xl font-bold mb-2`}>Matt Banner</h1>
+      <h1 className={`text-4xl font-bold mb-2`}>
+          <Link to="/">
+              Matt Banner
+          </Link>
+      </h1>
       <p className={`text-lg sm:text-xl mb-1 text-gray-600`}><span role="img" aria-label="Waving hand" className={`pr-1`}>👋🏼</span> Hello, I'm Matt, a Web Developer and AWS Architect based in the UK.</p>
       <p className={`text-gray-500`}>I work with AWS, Laravel, Craft CMS, Vue, Gatsby, Alpine &amp; Tailwind.</p>
       
