3dfe85cc5c3614c6cfe79902f9244ab5830514ac
Matt Banner <matt@banner.wtf>
2021-02-23 19:29:50 +0000

    Fix layout on 404 page
    Closes #11

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,15 +5,17 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404 - Not Found" />
+    <Layout>
+        <SEO title="404 - Not Found" />
 
-    <div>
-      <h1 className={`text-4xl font-bold mb-2`}>404 - Not Found</h1>
-      <p className={`text-lg`}>The page you requested could not be found.</p>
-      <Link to={`/`} className={`text-gray-500 hover:text-gray-800 transition duration-300 ease-in-out`}>Home</Link>
-    </div>
-  </Layout>
+        <main className={`max-w-3xl mx-auto w-full flex-grow flex items-center tracking-tight`}>
+            <div>
+                <h1 className={`text-4xl font-bold mb-2`}>404 - Not Found</h1>
+                <p className={`text-lg`}>The page you requested could not be found.</p>
+                <Link to={`/`} className={`text-gray-500 hover:text-gray-800 transition duration-300 ease-in-out`}>Home</Link>
+            </div>
+        </main>
+    </Layout>
 )
 
 export default NotFoundPage
