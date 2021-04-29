ab67d14cf68f79d82392175f4e138a9184502805
Matt Banner <matt.banner@enovate.co.uk>
2021-04-29 22:09:26 +0100

    Footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,16 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <footer className={`max-w-4xl mx-auto w-full text-center sm:text-left`}>
-      <div className={`mx-4 py-6 border-t text-secondary text-sm`}>
-        © {new Date().getFullYear()} Matt Banner. All rights reserved.
+    <footer className={`max-w-4xl mx-auto w-full border-t text-secondary text-sm text-center sm:text-left`}>
+      <div className="flex flex-col sm:flex-row justify-between mx-4 py-6">
+        <div className={``}>
+          © {new Date().getFullYear()} Matt Banner. All rights reserved.
+        </div>
+        <div className={`pt-6 mx-8 sm:mx-0 sm:pt-0 grid grid-cols-3 gap-4`}>
+            <div>Link 1</div>
+            <div>Link 2</div>
+            <div>Link 3</div>
+        </div>
       </div>
     </footer>
   )
