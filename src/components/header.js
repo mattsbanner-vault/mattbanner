0d2363d00f1cf8fdc89bf04a95ea3d6279321a29
Matt Banner <matt@banner.wtf>
2021-05-02 17:35:05 +0100

    Working nav static query

--- src/components/header.js
+++ src/components/header.js
@@ -4,15 +4,12 @@ import React from "react"
 import { Disclosure, } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
-// TODO: Load this from globals
-const navigation = [
-  { name: 'About', href: '/about', current: true },
-  { name: 'Articles', href: '/articles', current: false },
-  { name: 'Contact', href: '/contact', current: false },
-]
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
+}
+
+function isCurrent(uri) {
+  return false;
 }
 
 export default function Header({ entry }) {
@@ -20,26 +17,20 @@ export default function Header({ entry }) {
     <StaticQuery
       query={graphql`
         query {
-          entry: craftNotFoundNotFoundEntry {
-            id
-            remoteId
-            title
-            postDate
-            slug
-            bodyContent {
-              ...LeadTextFragment
-              ...RichTextFragment
-              ...ButtonFragment
-              ...HeadingFragment
+          primaryNavigation: craftNavigationGlobalSet {
+            items: primaryNavigationItems {
+              id
+              uri
+              title
             }
           }
         }
       `}
+
       render={data => (
-        <Disclosure as="nav">
-      {({ open }) => (
+      <Disclosure as="nav">
+        {({ open }) => (
         <>
-    <p>title {data.title}</p>
           <div className="max-w-4xl mx-auto px-2 sm:px-4 py-2">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -61,17 +52,17 @@ export default function Header({ entry }) {
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
-                    {navigation.map((item) => (
+                    {data.primaryNavigation.items.map((item) => (
                       <Link
-                        key={item.name}
-                        to={item.href}
+                        key={item.id}
+                        to={`/` + item.uri}
                         className={classNames(
-                          item.current ? 'bg-primary text-primary-copy' : 'text-gray-500 hover:bg-primary-lightest hover:text-primary-copy ',
+                            isCurrent(item.uri) ? 'bg-primary text-primary-copy' : 'text-gray-500 hover:bg-primary-lightest hover:text-primary-copy ',
                           'px-3 py-2 rounded-md text-sm font-medium transition duration-300 ease-in-out'
                         )}
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={isCurrent(item.uri) ? 'page' : undefined}
                       >
-                        {item.name}
+                        {item.title}
                       </Link>
                     ))}
                   </div>
@@ -82,17 +73,17 @@ export default function Header({ entry }) {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
+              {data.primaryNavigation.items.map((item) => (
                 <Link
-                  key={item.name}
-                  to={item.href}
+                  key={item.id}
+                  to={`/` + item.uri}
                   className={classNames(
-                    item.current ? 'bg-primary text-primary-copy' : 'text-gray-500 hover:bg-primary-lightest hover:text-primary-copy',
+                      isCurrent(item.uri) ? 'bg-primary text-primary-copy' : 'text-gray-500 hover:bg-primary-lightest hover:text-primary-copy',
                     'block px-3 py-2 rounded-md text-base font-medium transition duration-300 ease-in-out'
                   )}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={isCurrent(item.uri) ? 'page' : undefined}
                 >
-                  {item.name}
+                  {item.title}
                 </Link>
               ))}
             </div>
